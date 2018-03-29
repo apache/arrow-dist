25ee07d73df7f9e41274cc31f908d53e9bdb699d
Korn, Uwe <Uwe.Korn@blue-yonder.com>
2018-03-29 18:29:37 +0200

    Fix Boost RPATHs

--- python-macos/config.sh
+++ python-macos/config.sh
@@ -22,11 +22,16 @@ set -e
 function build_wheel {
     pip install -U pip
     pip install setuptools_scm
-    echo `pwd`
+
+    # Include brew installed versions of flex and bison.
+    # We need them to build Thrift. The ones that come with Xcode are too old.
     export PATH="/usr/local/opt/flex/bin:/usr/local/opt/bison/bin:$PATH"
+
+    echo `pwd`
     echo CFLAGS=${CFLAGS}
     echo CXXFLAGS=${CXXFLAGS}
     echo LDFLAGS=${LDFLAGS}
+
     pushd $1
 
     boost_version="1.65.1"
@@ -40,19 +45,24 @@ function build_wheel {
     arrow_boost="$PWD/arrow_boost"
     arrow_boost_dist="$PWD/arrow_boost_dist"
     mkdir "$arrow_boost" "$arrow_boost_dist"
-    pushd "${boost_directory_name}"
 
     # Arrow is 64-bit-only at the moment
     export CFLAGS="-fPIC -arch x86_64 ${CFLAGS//"-arch i386"/}"
     export CXXFLAGS="-fPIC -arch x86_64 ${CXXFLAGS//"-arch i386"} -std=c++11"
 
+    # Build Boost's bcp tool to create a custom namespaced boost build.
+    # Using this build, we can dynamically link our own boost build and
+    # don't need to fear any clashes with system / thirdparty provided versions
+    # of Boost.
+    pushd "${boost_directory_name}"
     ./bootstrap.sh
     ./b2 tools/bcp > /dev/null 2>&1
     ./dist/bin/bcp --namespace=arrow_boost --namespace-alias \
         filesystem date_time system regex build algorithm locale format \
 	"$arrow_boost" > /dev/null 2>&1
-
     popd
+
+    # Now build our custom namespaced Boost version.
     pushd "$arrow_boost"
     ./bootstrap.sh
     ./bjam cxxflags="${CXXFLAGS}" \
@@ -65,13 +75,23 @@ function build_wheel {
         install > /dev/null 2>&1
     popd
 
-    pushd "${arrow_boost_dist}"
+    # The boost libraries don't set an explicit install name and we have not
+    # yet found the correct option on `bjam` to set the install name to the
+    # one we desire.
+    #
+    # Set it to @rpath/<binary_name> so that they are search in the same
+    # directory as the library that loaded them.
+    pushd "${arrow_boost_dist}"/lib
       for dylib in *.dylib; do
-        install_name_tool -id @loader_path/${dylib} ${dylib}
+        install_name_tool -id @rpath/${dylib} ${dylib}
       done
+      install_name_tool -change libarrow_boost_system.dylib @rpath/libarrow_boost_system.dylib libarrow_boost_filesystem.dylib
     popd
 
-    export THRIFT_HOME=/usr/local
+    # We build a custom version of thrift instead of using the one that comes
+    # with brew as we also want it to use our namespaced version of Boost.
+    # TODO(PARQUET-1262): Use the external project facilities of parquet-cpp.
+    export THRIFT_HOME=`pwd`/thift-dist
     export THRIFT_VERSION=0.11.0
     wget http://archive.apache.org/dist/thrift/${THRIFT_VERSION}/thrift-${THRIFT_VERSION}.tar.gz
     tar xf thrift-${THRIFT_VERSION}.tar.gz
@@ -99,8 +119,12 @@ function build_wheel {
     popd
     popd
 
-    export ARROW_HOME=/usr/local
-    export PARQUET_HOME=/usr/local
+    # Now we can start with the actual build of Arrow and Parquet.
+    # We pin NumPy to an old version here as the NumPy version one builds
+    # with is the oldest supported one. Thanks to NumPy's guarantees our Arrow
+    # build will also work with newer NumPy versions.
+    export ARROW_HOME=`pwd`/arrow-dist
+    export PARQUET_HOME=`pwd`/arrow-dist
     pip install "cython==0.27.3" "numpy==${NP_TEST_DEP}"
     pushd cpp
     mkdir build
@@ -110,10 +134,9 @@ function build_wheel {
           -DARROW_BUILD_TESTS=OFF \
           -DARROW_BUILD_SHARED=ON \
           -DARROW_BOOST_USE_SHARED=ON \
-          -DARROW_JEMALLOC=OFF \
+          -DARROW_JEMALLOC=ON \
           -DARROW_PLASMA=ON \
           -DARROW_RPATH_ORIGIN=ON \
-          -DARROW_JEMALLOC_USE_SHARED=OFF \
           -DARROW_PYTHON=ON \
           -DARROW_ORC=ON \
           -DBOOST_ROOT="$arrow_boost_dist" \
@@ -142,8 +165,13 @@ function build_wheel {
     popd
     popd
 
+    # Unset the HOME variables and use pkg-config to discover the previously
+    # built binaries. By using pkg-config, we also are able to discover the
+    # ABI and SO versions of the dynamic libraries.
+    export PKG_CONFIG_PATH=${ARROW_HOME}/lib/pkgconfig:${PARQUET_HOME}/lib/pkgconfig:${PKG_CONFIG_PATH}
     unset ARROW_HOME
     unset PARQUET_HOME
+
     export PYARROW_WITH_PARQUET=1
     export PYARROW_WITH_ORC=1
     export PYARROW_WITH_JEMALLOC=1
@@ -159,6 +187,10 @@ function build_wheel {
            --bundle-arrow-cpp --bundle-boost --boost-namespace=arrow_boost \
            bdist_wheel
     ls -l dist/
+
+    # Do a test installation of the built wheel and change into another
+    # directory to ensure our import tests later on pick up the wheel and
+    # not the binaries from the build directory.
     for wheel in dist/*.whl; do
       pip install "$wheel"
       unzip -l "$wheel"
